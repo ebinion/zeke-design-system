aa191bd7631fc1b6f2f8a83dd9aca3b8b34a302c
Zeke Binion <zeke@binion.io>
2020-03-09 23:28:21 -0400

    chore: update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,1 +1,12 @@
-module.exports = ['@storybook/preset-create-react-app']
+module.exports = {
+  stories: ['../src/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-viewport/register',
+  ],
+}
