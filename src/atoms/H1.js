a46aab7646c5284f90329800396da8f072799b73
Zeke Binion <zeke@binion.io>
2020-12-08 11:03:47 -0500

    design: update heading fonts

--- src/atoms/H1.js
+++ src/atoms/H1.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 
 import Text from './Text'
 
-const H1 = props => <Text {...props} kind="heading" />
+const H1 = props => <Text kind={props.kind} {...props} />
 
 H1.propTypes = {
-  bold: PropTypes.bool,
-  size: PropTypes.oneOf(['xxl', 'xl', 'l', 'm', 's', 'xs']),
   as: PropTypes.string,
+  bold: PropTypes.bool,
+  kind: PropTypes.oneOf(['normal', 'heading', 'decorative', 'code']),
+  lineHeight: PropTypes.oneOf(['normal', 'tight']),
+  size: PropTypes.oneOf(['xxl', 'xl', 'l', 'm', 's', 'xs']),
 }
 
 H1.defaultProps = {
-  bold: true,
-  size: 'xxl',
   as: 'h1',
+  bold: true,
+  kind: 'heading',
+  lineHeight: 'tight',
+  size: 'xxl',
 }
 
 export default H1
--- src/atoms/H3.js
+++ src/atoms/H3.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 
 import Text from './Text'
 
-const H3 = props => <Text {...props} kind="heading" />
+const H3 = props => <Text kind={props.kind} {...props} />
 
 H3.propTypes = {
-  bold: PropTypes.bool,
-  size: PropTypes.oneOf(['xxl', 'xl', 'l', 'm', 's', 'xs']),
   as: PropTypes.string,
+  bold: PropTypes.bool,
+  kind: PropTypes.oneOf(['normal', 'heading', 'decorative', 'code']),
+  lineHeight: PropTypes.oneOf(['normal', 'tight']),
+  size: PropTypes.oneOf(['xxl', 'xl', 'l', 'm', 's', 'xs']),
 }
 
 H3.defaultProps = {
-  bold: true,
-  size: 'l',
   as: 'h3',
+  bold: true,
+  kind: 'heading',
+  lineHeight: 'tight',
+  size: 'l',
 }
 
 export default H3
