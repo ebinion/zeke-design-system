2e4db85c544311b017cd37c7983a22c295b2f71d
Zeke Binion <zeke@binion.io>
2020-12-08 15:34:00 -0500

    feat: update link styles

--- src/templates/GlobalStyles.js
+++ src/templates/GlobalStyles.js
@@ -6,12 +6,17 @@ import { colorTokens, textTokens } from '..'
 const GlobalWrapper = createGlobalStyle`
   body {
     background-color: ${colorTokens.backgrounds.site};
+    color: ${colorTokens.text.normal};
     font-family: ${textTokens.normal.fontFamily};
     font-size: ${textTokens.sizes.m.size};
     font-weight: ${textTokens.normal.weightNormal};
     line-height: ${textTokens.sizes.m.lineHeightTight};
     margin: 0;
     padding: 0;
+  }
+
+  :focus {
+    outline: 4px solid ${colorTokens.borders.link};
   }
 `
 
