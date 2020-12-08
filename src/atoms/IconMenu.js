98bf0ce25a62d19450ea9842fd69c54d50812449
Zeke Binion <zeke@binion.io>
2020-12-08 16:30:38 -0500

    design: update menu icon

--- src/atoms/IconMenu.js
+++ src/atoms/IconMenu.js
@@ -5,10 +5,7 @@ import Icon from './Icon'
 const IconMenu = props => {
   return (
     <Icon {...props} title="Menu" viewbox="0 0 32 32">
-      <path
-        d="M0 0v3h32v-3h-32zm0 10v3h32v-3h-32zm0 10v3h32v-3h-32z"
-        transform="translate(0 4)"
-      />
+      <path d="M0,7 L0,9.34782609 L25.6,9.34782609 L25.6,7 L0,7 Z M6.4,14.826087 L6.4,17.173913 L32,17.173913 L32,14.826087 L6.4,14.826087 Z M0,22.6521739 L0,25 L25.6,25 L25.6,22.6521739 L0,22.6521739 Z" fill-rule="nonzero" />
     </Icon>
   )
 }
