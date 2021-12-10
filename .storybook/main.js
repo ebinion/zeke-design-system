d5781e87f16176280e7c7c0cd991463dbbc1eff2
Zeke Binion <zeke@binion.io>
2021-12-10 03:38:08 -0500

    chore: clean up commented out code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
-    // '@storybook/addon-docs',
     '@storybook/preset-create-react-app',
     '@storybook/addon-a11y',
-    // '@storybook/addon-actions',
     '@storybook/addon-knobs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    // '@storybook/addon-viewport',
   ],
 }
