1eea3ef511502102ffab9ca7525e148d756916b1
Zeke Binion <zeke@binion.io>
2021-12-10 02:33:11 -0500

    fix: animations work again

--- src/molecules/Animate.stories.js
+++ src/molecules/Animate.stories.js
@@ -1,5 +1,4 @@
 import React from 'react' // eslint-disable-line no-unused-vars
-import { select, boolean } from '@storybook/addon-knobs'
 
 import { Animate } from '..'
 
@@ -7,28 +6,16 @@ const story = {
   title: 'Molecules/Animate',
   component: Animate,
 }
-
-const animationKnob = () =>
-  select(
-    'Animation kind',
-    [
-      'fadeIn',
-      'fadeInFromLeft',
-      'fadeInFromTop',
-      'fadeInFromRight',
-      'fadeInFromBottom',
-    ],
-    'fadeIn'
-  )
-
-const isPlayingKnob = () => boolean('Is playing?', true)
-
-export const animate = () => {
-  return (
-    <Animate kind={animationKnob()} isPlaying={isPlayingKnob()}>
-      <div>Example Animation</div>
-    </Animate>
-  )
-}
-
 export default story
+
+const Template = args => (
+  <Animate {...args}>
+    <div>Example Animation</div>
+  </Animate>
+)
+
+export const animate = Template.bind({})
+animate.args = {
+  kind: 'fadeIn',
+  isPlaying: false,
+}
