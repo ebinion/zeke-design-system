4c8877045081d9929b7518f00c3c76123188b4fd
Zeke Binion <zeke@binion.io>
2022-01-12 16:58:36 -0500

    design: bring Section inline with new design

--- src/organisms/Section.js
+++ src/organisms/Section.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Block, SectionHeader, measurementTokens } from '../'
+import { Block, SectionHeader } from '../'
 
 const Section = ({ superTitle, title, lead, children, isDark }) => {
   return (
@@ -9,16 +9,18 @@ const Section = ({ superTitle, title, lead, children, isDark }) => {
       as="section"
       color={isDark ? 'dark' : 'white'}
       padding="none"
-      paddingBottom={measurementTokens.componentPadding.xl}
+      paddingBottom="var(--component-padding-xl)"
     >
-      <SectionHeader
-        title={title}
-        superTitle={superTitle}
-        isKnockedout={isDark}
-        as="header"
-      >
-        {lead}
-      </SectionHeader>
+      <Block padding="xl" constrain="text" isCentered>
+        <SectionHeader
+          title={title}
+          superTitle={superTitle}
+          isKnockedout={isDark}
+          as="header"
+        >
+          {lead}
+        </SectionHeader>
+      </Block>
       {children}
     </Block>
   )
